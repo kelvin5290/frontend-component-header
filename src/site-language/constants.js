f1b9fe54c0074f6a3c45fcc84f19ef4d1d40ea4f
kelvin <kelvin.lok@greenradar.com>
2024-05-07 05:53:36 +0000

    add language

---
+++ src/site-language/constants.js
@@ -0,0 +1,19 @@
+const siteLanguageList = [
+  {
+    code: 'en',
+    name: 'English',
+    released: true,
+  },
+  {
+    code: 'zh-cn',
+    name: '中文 (简体)',
+    released: true,
+  },
+ {
+    code: 'zh-hk',
+    name: '中文 (繁體)',
+    released: true,
+  }
+];
+
+export default siteLanguageList;
